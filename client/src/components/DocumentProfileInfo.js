369752860cc244852c1d1cab1e78f1a912cc7445
Cela24k <882778@stud.unive.it>
2021-12-06 20:43:18 +0100

    Modificato DocumentProfileInfo
    Aggiunta immagine e stile

--- client/src/components/DocumentProfileInfo.js
+++ client/src/components/DocumentProfileInfo.js
@@ -28,14 +28,34 @@ class DocumentProfileInfo extends React.Component {
 		reader.readAsArrayBuffer(file);
 	};
 
+	//implementare il size massimo di 256MB o quello che era
+	//come parametro number ci va file.size
+	returnFileSize(number) {
+		if (number < 1024) {
+			return number + 'bytes';
+		} else if (number >= 1024 && number < 1048576) {
+			return (number / 1024).toFixed(1) + 'KB';
+		} else if (number >= 1048576) {
+			return (number / 1048576).toFixed(1) + 'MB';
+		}
+	}
+
 	render() {
 		const { myProfile, profileId, documents } = this.state;
 		// const texts = Texts[language].profileDocuments;
 		return (
 			<React.Fragment>
+				<div style={divStyle} >
+					{myProfile && (
+						<i class="fas fa-file-upload fa-10x"
+							style={labelStyle}>
+						</i>
+					)}
+				</div>
 				<div className="addChildPrompt">
 					{myProfile && (
 						<input
+							style={fileInput}
 							id="get-document"
 							type="file"
 							onChange={this.readFile}
@@ -46,6 +66,23 @@ class DocumentProfileInfo extends React.Component {
 			</React.Fragment>
 		);
 	}
+}
+
+const divStyle = {
+	"margin-top":"100px",
+	height: "100%",
+	display: "flex",
+	"justify-content": "center",
+	"align-items": "center"
+}
+
+const fileInput = {
+	opacity: 0.5,
+	"padding-left": "50px"
+}
+
+const labelStyle = {
+	opacity: 0.4
 }
 
 export default withLanguage(DocumentProfileInfo);
