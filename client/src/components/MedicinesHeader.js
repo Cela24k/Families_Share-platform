a4fc7b211e09e7807d636ea298fc65dcbafbf853
Mimiview <57045037+Mimiview@users.noreply.github.com>
2021-12-07 17:15:04 +0100

    aggiunta immagine nel in medicineProfileScreen

--- client/src/components/MedicinesHeader.js
+++ client/src/components/MedicinesHeader.js
@@ -12,72 +12,88 @@ import OptionsModal from "./OptionsModal";
 
 
 class MedicinesHeader extends React.Component {
-        // da inziare by mimmo 
-      handleEdit = () => {
-        alert("Devo essere implementato");
-      };
-    
-      handleOptions = () => {
-        alert("Devo essere implementato");
-      };
-    
-      
-    
-      render() {
-        const { language, match, history, photo, name } = this.props;
-        const { profileId } = match.params;
-        const texts = Texts[language].profileHeader;
-        
+  state = {
+    optionsModalIsOpen: false,
+    confirmDialogIsOpen: false,
+    imageModalIsOpen: false,
+  };
 
-        return (
-          <div id="profileHeaderContainer">
-            <div className="row no-gutters" id="profileHeaderOptions">
-              <div className="col-2-10">
+  handleImageModalOpen = () => {
+    const target = document.querySelector(".ReactModalPortal");
+    disableBodyScroll(target);
+    this.setState({ imageModalIsOpen: true });
+  };
+
+  handleImageModalClose = () => {
+    clearAllBodyScrollLocks();
+    this.setState({ imageModalIsOpen: false });
+  };
+  // da inziare by mimmo 
+  handleEdit = () => {
+    alert("Devo essere implementato");
+  };
+
+  handleOptions = () => {
+    alert("Devo essere implementato");
+  };
+
+
+
+  render() {
+    const { language, match, history, photo } = this.props;
+    const { profileId } = match.params;
+    const texts = Texts[language].profileHeader; // questo vedere se utilizzarli o meno 
+    const { imageModalIsOpen } = this.setState;
+
+    return (
+      <div id="profileHeaderContainer">
+        <div className="row no-gutters" id="profileHeaderOptions">
+          <div className="col-2-10">
+            <button
+              type="button"
+              className="transparentButton center"
+              onClick={() => history.goBack()}
+            >
+              <i className="fas fa-arrow-left" />
+            </button>
+          </div>
+          <div className="col-6-10" />
+          {profileId === JSON.parse(localStorage.getItem("user")).id ? ( //todo bisogna vedere in che modo viene settato lo user nel localStorage
+            <React.Fragment>
+              <div className="col-1-10">
                 <button
                   type="button"
                   className="transparentButton center"
-                  onClick={() => history.goBack()}
+                  onClick={this.handleEdit}
                 >
-                  <i className="fas fa-arrow-left" />
+                  <i className="fas fa-pencil-alt" />
                 </button>
               </div>
-              <div className="col-6-10" />
-              {profileId === JSON.parse(localStorage.getItem("user")).id ? (
-                <React.Fragment>
-                  <div className="col-1-10">
-                    <button
-                      type="button"
-                      className="transparentButton center"
-                      onClick={this.handleEdit}
-                    >
-                      <i className="fas fa-pencil-alt" />
-                    </button>
-                  </div>
-                  <div className="col-1-10">
-                    <button
-                      type="button"
-                      className="transparentButton center"
-                      onClick={this.handleOptions}
-                    >
-                      <i className="fas fa-ellipsis-v" />
-                    </button>
-                  </div>
-                </React.Fragment>
-              ) : (
-                <div />
-              )}
-            </div>
-            <img
-              className="profilePhoto horizontalCenter"
-              alt="user's profile"
-              src={photo}
-              onClick={this.handleImageModalOpen}
-            />
-            <h1 className="horizontalCenter">Prossima Medicina da prendere</h1>
-           
-          </div>
-        );
-      }
-    }
+              <div className="col-1-10">
+                <button
+                  type="button"
+                  className="transparentButton center"
+                  onClick={this.handleOptions}
+                >
+                  <i className="fas fa-ellipsis-v" />
+                </button>
+              </div>
+            </React.Fragment>
+          ) : (
+            <div />
+          )}
+        </div>
+        <img
+          className="profilePhoto horizontalCenter"
+          alt="user's profile"
+          src={photo}
+          onClick={this.handleImageModalOpen}
+        />
+        <h1 className="horizontalCenter">Prossima Medicina da prendere</h1>
+
+      </div>
+    );
+  }
+}
 
 export default withRouter(withLanguage(MedicinesHeader));
--- client/src/components/MedicinesProfileScreen.js
+++ client/src/components/MedicinesProfileScreen.js
@@ -1,15 +1,58 @@
 import React from "react";
 import MedicinesHeader from "./MedicinesHeader";
+import * as path from "lodash.get";
+import LoadingSpinner from "./LoadingSpinner";
+import Log from "./Log";
+import axios from "axios";
 
+const getMyProfile = async (userId) => {
+    try {
+        const response = await axios.get(`/api/users/${userId}/profile`);
+        return response.data;
+    } catch (error) {
+        Log.error(error);
+        return {
+            given_name: "",
+            family_name: "",
+            image: { path: "/images/profiles/user_default_photo.png" },
+            address: { street: "", number: "" },
+            email: "",
+            phone: "",
+            phone_type: "",
+            visible: false,
+            user_id: "",
+        };
+    }
+};
 
 class MedicinesProfileScreen extends React.Component {
 
+    state = {
+        profile : {},
+        fetchedProfile : false,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+        const { profileId } = match.params;
+        const profile = await getMyProfile(profileId);
+        this.setState({
+            profile,
+            fetchedProfile: true,
+        });
+    }
     render() {
-        return (
+        const { match } = this.props;
+        const { profileId } = match.params;
+        const { profile, documents, fetchedProfile } = this.state;
+        return fetchedProfile ? (
             <React.Fragment>
                 <MedicinesHeader
+                    photo={path(profile, ["image", "path"])}
                 /> {/*L'unica cosa da vedere è come mandargli laa foto da props */}
             </React.Fragment>
+        ) : ( 
+            <LoadingSpinner/>
         );
     }
 }
