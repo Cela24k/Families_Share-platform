fd43eb03d5243fe96559537ba29d736f7b0cd265
Leonardo <leo.dechi99@gmail.com>
2022-01-11 15:59:00 +0100

    mini fix

--- client/src/components/DocumentProfileInfo.js
+++ client/src/components/DocumentProfileInfo.js
@@ -12,141 +12,138 @@ import DocumentListItem from "./DocumentListItem";
 
 class DocumentProfileInfo extends React.Component {
 
-	constructor(props) {
-		super(props);
-		const userId = JSON.parse(localStorage.getItem("user")).id;
-		const { profileId, userDocuments } = this.props;
-		const myProfile = userId === profileId;
-		this.state = {
-			myProfile,
-			documents: userDocuments,
-			profileId,
-		};
-	}
+  constructor(props) {
+    super(props);
+    const userId = JSON.parse(localStorage.getItem("user")).id;
+    const { profileId, userDocuments } = this.props;
+    const myProfile = userId === profileId;
+    this.state = {
+      myProfile,
+      documents: userDocuments,
+      profileId,
+    };
+  }
 
-	readFile = event => {
-		const { profileId } = this.state;
-		const file = document.getElementById("input").files[0];
-		const reader = new FileReader();
-		console.log(event);
-		reader.onload = () => {
-			axios
-				.post(`/api/users/${profileId}/health/documents`, {
-					"filename": file.name,
-					"filedata": reader.result
-				})
-				.then((response) => {
-					/*trovare un metodo più elegante, spoiler: esiste ed è quello di aggiornare lo state della component genitore \
-					in questo caso suppongo bisogni aggiornare lo state sia della singola componente DocumentList, sia lo stato di \
-					DocumentProfileInfo */
-					//aggiunto il metodo per reloadare la pagina quando viene caricato un documento accettato
-					window.location.reload(false);
-					Log.info(response);
-				})
-				.catch((error) => {
-					Log.error(error);
-				})
-		}
-		reader.readAsDataURL(file);
-	};
+  readFile = () => {
+    const { profileId } = this.state;
+    const file = document.getElementById("input").files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      axios
+        .post(`/api/users/${profileId}/health/documents`, {
+          "filename": file.name,
+          "filedata": reader.result
+        })
+        .then((response) => {
+          /*trovare un metodo più elegante, spoiler: esiste ed è quello di aggiornare lo state della component genitore \
+          in questo caso suppongo bisogni aggiornare lo state sia della singola componente DocumentList, sia lo stato di \
+          DocumentProfileInfo */
+          //aggiunto il metodo per reloadare la pagina quando viene caricato un documento accettato
+          window.location.reload(false);
+          Log.info(response);
+        })
+        .catch((error) => {
+          Log.error(error);
+        })
+    }
+    reader.readAsDataURL(file);
+  };
 
-	//implementare il size massimo di 256MB o quello che era
-	//come parametro number ci va file.size
-	returnFileSize(number) {
-		if (number < 1024) {
-			return number + 'bytes';
-		} else if (number >= 1024 && number < 1048576) {
-			return (number / 1024).toFixed(1) + 'KB';
-		} else if (number >= 1048576) {
-			return (number / 1048576).toFixed(1) + 'MB';
-		}
-	}
+  //implementare il size massimo di 256MB o quello che era
+  //come parametro number ci va file.size
+  returnFileSize(number) {
+    if (number < 1024) {
+      return number + 'bytes';
+    } else if (number >= 1024 && number < 1048576) {
+      return (number / 1024).toFixed(1) + 'KB';
+    } else if (number >= 1048576) {
+      return (number / 1048576).toFixed(1) + 'MB';
+    }
+  }
 
-	render() {
-		const { myProfile, profileId, documents } = this.state;
-		const { profile, userChildren } = this.props;
-		// const texts = Texts[language].profileDocuments;
-		return (
-			<React.Fragment>
-				{myProfile && documents.length > 0 ? (
-					<ul>
-						{documents.map((_document, index) => (
-							<li key={index}>
-								{/* passato l'index della document map per usarlo come prop*/}
-								<DocumentListItem
-									userId={profileId}
-									keyId={index}
-								/>
-							</li>
-						))}
-					</ul>
-				) : (
-					<div style={divStyle} >
-						<i className="fas fa-file-upload fa-10x" style={labelStyle} />
-					</div>
-				)}
-				<div className="addChildPrompt">
-					<input
-						style={fileInput}
-						id="input"
-						type="file"
-						onChange={this.readFile}
-					/>
-					{myProfile && (
-						<Fab
-							mainButtonStyles={mainButtonStyles}
-							alwaysShowTitle={true}
-							icon={<FontAwesomeIcon icon={faPlus} />}
-						>
-							<Action
-								style={actionStyles}
-								text={profile.given_name}
-								onClick={() => document.getElementById('input').click()}
-							>
-								<FontAwesomeIcon icon={faUser} />
-							</Action>
-							{userChildren.map(child => (
-								<Action
-									style={actionStyles}
-									text={child.child_id}
-									onClick={() => document.getElementById('input').click()}
-								>
-									<FontAwesomeIcon icon={faChild} />
-								</ Action>
-							))}
-						</Fab>
-					)}
-				</div>
-				<div className="addChildPrompt" id="document-data" />
-			</React.Fragment>
-		);
-	}
+  render() {
+    const { myProfile, profileId, documents } = this.state;
+    const { profile, userChildren } = this.props;
+    // const texts = Texts[language].profileDocuments;
+    return (
+      <React.Fragment>
+        {myProfile && documents.length > 0 ? (
+          <ul>
+            {documents.map((_document, index) => (
+              <li key={index}>
+                <DocumentListItem
+                  userId={profileId}
+                  keyId={index}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div style={divStyle} >
+            <i className="fas fa-file-upload fa-10x" style={labelStyle} />
+          </div>
+        )}
+        <div>
+          <input
+            style={fileInput}
+            id="input"
+            type="file"
+            onChange={this.readFile}
+          />
+          {myProfile && (
+            <Fab
+              mainButtonStyles={mainButtonStyles}
+              alwaysShowTitle={true}
+              icon={<FontAwesomeIcon icon={faPlus} />}
+            >
+              <Action
+                id={profileId}
+                style={actionStyles}
+                text={profile.given_name}
+                onClick={() => document.getElementById('input').click()}
+              >
+                <FontAwesomeIcon icon={faUser} />
+              </Action>
+              {userChildren.map(child => (
+                <Action
+                  id={child.child_id}
+                  style={actionStyles}
+                  text={child.given_name}
+                  onClick={() => document.getElementById('input').click()}
+                >
+                  <FontAwesomeIcon icon={faChild} />
+                </Action>
+              ))}
+            </Fab>
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 const mainButtonStyles = {
-	backgroundColor: "#ff6f00",
+  backgroundColor: "#ff6f00",
 }
 
 const actionStyles = {
-	backgroundColor: "#ff8c00",
+  backgroundColor: "#ff8c00",
 };
 
 const divStyle = {
-	"marginTop": "100px",
-	height: "100%",
-	display: "flex",
-	"justifyContent": "center",
-	"alignItems": "center"
+  "marginTop": "100px",
+  height: "100%",
+  display: "flex",
+  "justifyContent": "center",
+  "alignItems": "center"
 }
 
 const fileInput = {
-	opacity: 0.6,
-	// "paddingLeft": "50px"
-	display: "none"
+  display: "none"
 }
 
 const labelStyle = {
-	opacity: 0.4
+  opacity: 0.4
 }
 
 export default withLanguage(DocumentProfileInfo);
--- client/src/components/DocumentProfileScreen.js
+++ client/src/components/DocumentProfileScreen.js
@@ -9,98 +9,123 @@ import LoadingSpinner from "./LoadingSpinner";
 
 /* richiede i documenti al server */
 const getMyDocuments = async (userId) => {
-    try {
-        const response = await axios
-            .get(`/api/users/${userId}/health/documents`);
-        return response.data;
-    } catch (error) {
-        Log.error(error);
-        return [];
-    }
+  try {
+    const response = await axios
+      .get(`/api/users/${userId}/health/documents`);
+    return response.data;
+  } catch (error) {
+    Log.error(error);
+    return [];
+  }
 };
 
 /* richiede le informazioni al server del profilo dello user attualmente connesso */
 const getMyProfile = async (userId) => {
-    try {
-        const response = await axios.get(`/api/users/${userId}/profile`);
-        return response.data;
-    } catch (error) {
-        Log.error(error);
-        return {
-            given_name: "",
-            family_name: "",
-            image: { path: "/images/profiles/user_default_photo.png" },
-            address: { street: "", number: "" },
-            email: "",
-            phone: "",
-            phone_type: "",
-            visible: false,
-            user_id: "",
-        };
-    }
+  try {
+    const response = await axios.get(`/api/users/${userId}/profile`);
+    return response.data;
+  } catch (error) {
+    Log.error(error);
+    return {
+      given_name: "",
+      family_name: "",
+      image: { path: "/images/profiles/user_default_photo.png" },
+      address: { street: "", number: "" },
+      email: "",
+      phone: "",
+      phone_type: "",
+      visible: false,
+      user_id: "",
+    };
+  }
 };
 
 const getMyChildren = async (userId) => {
-    try {
-        const response = await axios
-            .get(`/api/users/${userId}/children`);
-        return response.data;
-    } catch (error) {
-        Log.error(error);
-        return [];
-    }
+  try {
+    const response = await axios
+      .get(`/api/users/${userId}/children`);
+    return response.data;
+  } catch (error) {
+    Log.error(error);
+    return [];
+  }
 };
+
+const getMyChildrenInfo = (userId, userChildren) => {
+  const childrenInfo = [];
+  userChildren.forEach(async child => {
+    await axios
+      .get(`/api/users/${userId}/children/${child.child_id}`)
+      .then(response => {
+        childrenInfo.push(response.data)
+      })
+      .catch(error => {
+        Log.error(error)
+        childrenInfo.push({
+          image: { path: "" },
+          birthdate: new Date(),
+          gender: "unspecified",
+          given_name: "",
+          family_name: "",
+          child_id: "",
+        })
+      })
+  })
+  return childrenInfo;
+}
 
 class DocumentProfileScreen extends React.Component {
 
-    state = {
-        profile: {},
-        documents: [],
-        children: [],
-        fetchedProfile: false,
-    };
+  state = {
+    profile: {},
+    documents: [],
+    childrenInfo: [],
+    fetchedProfile: false,
+  };
 
-    async componentDidMount() {
-        const { match } = this.props;
-        const { profileId } = match.params;
-        const userDocuments = await getMyDocuments(profileId);
-        const userChildren = await getMyChildren(profileId);
-        const profile = await getMyProfile(profileId);
-        // const children = await getMyChildren(profileId);
-        this.setState({
-            profile,
-            documents: userDocuments,
-            children: userChildren,
-            fetchedProfile: true,
-        });
-    }
+  async componentDidMount() {
+    const { match } = this.props;
+    const { profileId } = match.params;
+    const userDocuments = await getMyDocuments(profileId);
+    const userChildren = await getMyChildren(profileId);
+    const childrenInfo = getMyChildrenInfo(profileId, userChildren);
+    const profile = await getMyProfile(profileId);
+    // const children = await getMyChildren(profileId);
+    console.log("childrenInfo: ", childrenInfo);
+    this.setState({
+      profile,
+      documents: userDocuments,
+      childrenInfo,
+      fetchedProfile: true,
+    });
+  }
 
-    render() {
-        const { match } = this.props;
-        const { profileId } = match.params;
-        const { profile, documents, children, fetchedProfile } = this.state;
-        // const texts = Texts[language].ProfileDocumentHeader;
-        return fetchedProfile ? (
-            <React.Fragment>
-                <DocumentProfileHeader
-                    name={`${profile.given_name} ${profile.family_name}`}
-                    photo={path(profile, ["image", "path"])}
-                />
-                <DocumentProfileInfo
-                    profile={profile}
-                    profileId={profileId}
-                    userDocuments={documents}
-                    userChildren={children}
-                />
-            </React.Fragment>
-        ) : (
-            <LoadingSpinner />
-        );
-    }
+  render() {
+    const { match } = this.props;
+    const { profileId } = match.params;
+    const { profile, documents, fetchedProfile, childrenInfo } = this.state;
+    // const texts = Texts[language].ProfileDocumentHeader;
+    return fetchedProfile ? (
+      <React.Fragment>
+        <DocumentProfileHeader
+          name={`${profile.given_name} ${profile.family_name}`}
+          photo={path(profile, ["image", "path"])}
+        />
+        <DocumentProfileInfo
+          profile={profile}
+          profileId={profileId}
+          userDocuments={documents}
+          userChildren={childrenInfo}
+        />
+      </React.Fragment>
+    ) : (
+      <LoadingSpinner />
+    );
+  }
 }
 
 DocumentProfileScreen.propTypes = {
-    documents: PropTypes.array
+  documents: PropTypes.array
 };
 
 export default DocumentProfileScreen;
